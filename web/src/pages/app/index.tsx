5a61ae3512ac50be3cad6ea25421fbed5fb33d63
thefalked <giulianocrivelli@hotmail.com>
2022-04-06 22:30:46 -0400

    feat: class 01 - projects base structure

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,20 @@
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import type { GetServerSideProps, NextPage } from "next";
+
+const Home: NextPage = () => {
+  const { user } = useUser();
+
+  return (
+    <div>
+      <h1>Hellow World </h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <a href="/api/auth/logout">Logout</a>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired();
