8ea1fd910069342f219c87cb0c13416fa69d27a6
thefalked <giulianocrivelli@hotmail.com>
2022-04-12 02:30:55 -0400

    class 4 - Integrating Front-end with Apollo client and server side; added tailwind

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,12 +1,19 @@
-import { getSession, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import type { GetServerSideProps, NextPage } from "next";
+import { useMeQuery } from "../../graphql/generated/graphql";
+import { ssrGetProducts } from "../../graphql/generated/page";
+import { withApollo } from "../../lib/withApollo";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ data }) => {
   const { user } = useUser();
+  const { data: me, loading, error } = useMeQuery();
 
   return (
-    <div>
+    <div className="text-violet-500">
       <h1>Hellow World </h1>
+      <pre>{JSON.stringify(me, null, 2)}</pre>
+
+      {/* <pre>{JSON.stringify(data.products, null, 2)}</pre> */}
 
       <pre>{JSON.stringify(user, null, 2)}</pre>
 
@@ -15,11 +22,11 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default withApollo(ssrGetProducts.withPage()(Home));
 
 export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ req, res }) => {
-    console.log(getSession(req, res));
+  getServerSideProps: async (ctx) => {
+    // return getServerPageGetProducts({}, ctx);
 
     return {
       props: {},
